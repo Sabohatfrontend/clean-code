42048fc9423beaf2592efd0ad1254ff475925891
Sabohatfrontend <ssobirova333@gmail.com>
2024-02-18 16:08:33 +0500

    fix (basic-3.3): change classes short name

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput = document.getElementById("new-task");//Add a new task.
 var addButton = document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder = document.getElementById("incomplete-tasks");//ul of #incompleteTasks
-var completedTasksHolder = document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder = document.getElementById("incomp-tasks");//ul of #incompleteTasks
+var completedTasksHolder = document.getElementById("comp-tasks");//completed-tasks
 
 
 //New task list item
@@ -43,7 +43,7 @@ var createNewTaskElement = function (taskString) {
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
   editButton.className = "edit";
 
-  deleteButton.className = "delete";
+  deleteButton.className = "del";
   deleteButtonImg.src = './remove.svg';
   deleteButton.appendChild(deleteButtonImg);
 
@@ -157,7 +157,7 @@ var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
   //select ListItems children
   var checkBox = taskListItem.querySelector("input[type=checkbox]");
   var editButton = taskListItem.querySelector("button.edit");
-  var deleteButton = taskListItem.querySelector("button.delete");
+  var deleteButton = taskListItem.querySelector("button.del");
 
 
   //Bind editTask to edit button.
